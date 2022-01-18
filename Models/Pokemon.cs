766ae05b625fe5eca46c8a44b9f0e5e185ee3317
LejusVDP <julvandeputte@gmail.com>
2022-01-18 18:38:29 +0100

    Ajoutez des fichiers projet.

---
+++ Models/Pokemon.cs
@@ -0,0 +1,24 @@
+﻿namespace Pokedex.Models
+{
+    internal class Pokemon
+    {
+        private string p_cry;
+        private string p_name;
+
+        public string Name
+        {
+            get { return p_name; }
+        }
+
+        public Pokemon(string n)
+        {
+            this.p_cry = string.Empty;
+            this.p_name = n;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("My name is {0}", this.p_name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Pokedex.Models;
+using System;
+
+namespace Pokedex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vive Linux !");
+
+            Pokemon pikachu = new Pokemon("Pika");
+
+            Console.WriteLine(pikachu.Name);
+            Console.WriteLine(pikachu.ToString());
+        }
+    }
+}
